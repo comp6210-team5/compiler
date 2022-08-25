127df15487f3975a06f0a9de1b38f32683ffb73e
Braden Tisdale <bnt0008@auburn.edu>
2022-08-25 17:24:21 -0500

    maybe regex will now work with multi-char words?

--- typing.py
+++ typing.py
@@ -1,12 +1,33 @@
 #for definitions, classes, and functions
 #that all or most other files will import
 
+_REGEX_ESCAPES = {
+	'(',
+	')',
+	'|',
+	'[',
+	']',
+	'+',
+	'*',
+	'$',
+	'^',
+	'.',
+	'?',
+	'{',
+	'}',
+	'\\'
+}
 
 #simple wrapper to avoid case-sensitivity
 #idk if we'll keep this, we may want case sensitivity
 class CaseInsensitiveSet:
 	def __init__(self, names):
 		self._SET = frozenset({name.upper() for name in names})
+		self.onechar = True
+		for name in names:
+			if len(name) != 1:
+				self.onechar = False
+				break
 	
 	def __contains__(self, item):
 		return item.upper() in self._SET
@@ -15,18 +36,16 @@ class CaseInsensitiveSet:
 		for name in self._SET:
 			yield name
 	
+	
 	def regex(self):
-		reg = r'['
+		reg = str()
 		for val in self._SET:
-			if val != ']' and val != '\\' and val != '^':
-				reg = reg + rf'{val}'
-		if ']' in self._SET:
-			reg = r'[]' + reg[1:]
-		if '\\' in self._SET:
-			reg = reg + '\\'
-		if '^' in self._SET:
-			reg = reg + r'^'
-		return reg + r']'
+			if val in _REGEX_ESCAPES:
+				reg = reg + '\\'
+			reg = reg + rf'{val}|'
+		return '(' + reg[:-1] + ')'
+	
+		
 	
 	@classmethod
 	def union(cls, *others):
@@ -56,8 +75,10 @@ ONECHAR_OPERATORS = CaseInsensitiveSet({
 	'!',
 	'&',
 	'|',
+	'[',
 	']',
-	'^'
+	'^',
+	'\\'
 })
 
 print(ONECHAR_OPERATORS.regex())
