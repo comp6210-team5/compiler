4f3d0fb286c2362031ece3b35641eee1de5251f6
Braden Tisdale <bnt0008@auburn.edu>
2022-08-29 10:04:52 -0500

    improved regex

--- tokenizer.py
+++ tokenizer.py
@@ -10,20 +10,25 @@ class Token:
 		self.typename = tp.typename(text)
 
 def tokenize(source, args):
-	#Reference: \d is for decimal numbers,
-	#[\d']+ matches 1 or more decimal numbers and ticks,
-	#\. matches a period,
-	#[\d']* matches any number of decimal numbers and ticks
-	#(...)? matches the parens 0 or 1 times
-	numbers = r"[\d']+(\.[\d']*)?"
+	#\d+ matches 1 or more decimal numbers,
+	#[\d']* matches 0 or more decimal numbers and ticks
+	decimals = r"\d+[\d']*"
 	
+	#\.{decimals})? matches 0 or 1 instances of a
+	#period followed by more decimals
+	numbers = rf'{decimals}(\.{decimals})?'
+	
+	#[^"] matches any character except "
 	#(\\\n)? matches 0 or 1 instances of a
 	#backslash followed by a line break
-	strings = r'"(.*(\\\n)?)+"'
+	strings = r'"([^"](\\\n)?)+"'
 	
-	comment = r'//(?m.*$)'
+	#.* does not match linebreaks
+	comment = r'//.*\n'
 	
-	multicomment = r'/\*(.*\n?)*\*/'
+	#(?m.*)*? matches any characters, including linebreaks,
+	#matching a minimal number (so until the first */)
+	multicomment = r'/\*(?m.*)*?\*/'
 	
 	symbols = re.compile(f'{comment}|{multicomment}|{strings}|{numbers}|' \
 											+ tp.ALL_SYMBOLS.regex())
