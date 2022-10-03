7b6241d7da5a41d6d540691395ed86b473177382
ti <cai0004@auburn.edu>
2022-10-03 08:11:38 -0500

    Cleaned up the exhaustive parser a bit and fixed some minor bugs, but ran into a major bug where recursively solving the rest of the reduction creates essentially new left-recursive issues in a non-left-recursive grammar.

--- parser.py
+++ parser.py
@@ -50,36 +50,7 @@ class Terminal(Node):
 class Reduction:
 	def __init__(self, *reduction):
 		self.reduction = reduction
-		self.choices = None
 		
-	# needs to be initialized after all of the reductions have been added
-	def _initChoices(self):
-		self.choices = [0 for _ in range(len(self.reduction))]
-		for i, r in enumerate(self.reduction):
-			assert isinstance(r, (Rule, Reduction, TYPES, str))
-			if isinstance(r, Rule):
-				# We assume all rules have at least one
-				# reduction. A rule matching an empty string may
-				# have a single, empty reduction.
-				assert(len(r.reductions) > 0)
-				self.choices[i] = len(r.reductions) - 1
-
-	# iterate exhaustively through all potential combinations of reduction
-	# choices among each of the rules in the top-level of this reduction
-	# (any nested reductions should handle their own iterative exhaustions
-	# using this as well)
-	def iterChoices(self, skip):
-		c = self.choices[skip:]
-		i = [0 for _ in range(len(c))]
-		yield i
-		while not all([j == k for j, k in zip(i, c)]):
-			j = len(c) - 1
-			i[j] = (i[j] + 1) % (c[j] + 1)
-			while i[j] == 0:
-				j -= 1
-				i[j] = (i[j] + 1) % (c[j] + 1)
-			yield i
-
 	# Returns a generator over all possible reduction matches under all
 	# possible derivation choices of any rules within the reduction.  The
 	# generator iterates over tuples of:
@@ -90,12 +61,6 @@ class Reduction:
 	# the match.
 	# if skip is > 0, then skip the first 'skip' parts of the reduction
 	def reduce(self, tokens, skip=0):
-		# We assume reduce will only be called on a reduction at a point
-		# once the grammar is finalized (where all rules in the
-		# reduction have been both instantiated and filled with
-		# reductions.
-		if self.choices is None:
-			self._initChoices()
 			
 		#We match the empty token (i.e., epsilon)
 		if len(self.reduction[skip:]) == 0:
@@ -106,50 +71,48 @@ class Reduction:
 			# iterations? Or will it yield this repeatedly?
 			yield [Terminal(None)], 0
 		else:
-			# For every possible choice of reductions of any sub rules (This
-			# loop handles iteration over all possible choices of top-level
-			# rules contained in this reduction, and repeatedly yields all
-			# available matches from any nested reduction or rule which are
-			# themselves recursively iterated during a particular step,
-			# provided that, for each step of the below iteration the nested
-			# iterations of nested rules and reductions start over, which I
-			# think is the case.
-			for choice_list in self.iterChoices(skip):
+			for i, r in enumerate(self.reduction[skip:]):
 
-				# current index in the token list
-				current = 0
-				for i, (choice, r) in enumerate(zip(choice_list, self.reduction[skip:])):
-					# TODO: do we need to wrap in a try-catch IndexError block?
-					if isinstance(r, Rule):
-						result = r.descend(tokens[current:], choice)
-					# nested reductions can be thought of as rules
-					# with a single reduction choice
-					elif isinstance(r, Reduction):
-						result = r.reduce(tokens[current:])
-					elif self._match_terminal(tokens[current], r):
-						result = [([Terminal(tokens[current])], 1)]
+				# TODO: do we need to wrap in a try-catch IndexError block?
+				if isinstance(r, Rule):
+					result = r.descend(tokens)
+				# nested reductions can be thought of as rules
+				# with a single reduction choice
+				elif isinstance(r, Reduction):
+					result = r.reduce(tokens)
+				elif len(tokens) > 0 and self._match_terminal(tokens[0], r):
+					result = [([Terminal(tokens[0])], 1)]
+				else:
+					result = []
+                                resultEmpty = True
+				# for each match and length of match (in tokens) in the result
+				for prefix, len_i in result:
+					resultEmpty = False
+					# TODO: if we skip all of the
+					# reductions, does it return an empty
+					# match successfully?
+					# UPDATE: apparently not?
+					if i+1 == len(self.reduction[skip:]):
+						yield prefix, len_i
 					else:
-						result = []
-					# for each match and length of match (in tokens) in the result
-					for prefix, len_i in result:
-						# TODO: if we skip all of the
-						# reductions, does it return an empty
-						# match successfully?
-						# UPDATE: apparently not?
-						if i+1 == len(self.reduction[skip:]):
-							yield prefix, len_i
-						# TODO: if calling self.reduce() inside
-						# of a super.reduce() call from
-						# OptionalReduction or
-						# RepetitionReduction, does it call the
-						# subclass reduction first, as I (the
-						# programmer) intend? Or does it call
-						# immediately back to super.reduce(),
-						# which will cause errors.
-						for tail, len_j in self.reduce(tokens[current+len_i:], i+1):
+                                                # n.b. this approach doesn't
+                                                # work as well as I'd hoped. The
+                                                # problem is we're essentially
+                                                # defining new grammar rules by
+                                                # skipping the first parts of a
+                                                # reduction, and with that we're
+                                                # creating new left-recursive
+                                                # rules that weren't
+                                                # left-recursive in the original
+                                                # grammar. This seems like a
+                                                # fundamental and unavoidable
+                                                # issue with this approach.
+						for tail, len_j in self.reduce(tokens[len_i:], i+1):
 							yield prefix + tail, len_i + len_j
-						
-	
+
+				if resultEmpty:
+					break
+				
 	#Convenience function to match either token types or literal values
 	def _match_terminal(self, token, r):
 		if isinstance(r, str):
@@ -162,7 +125,7 @@ class OptionalReduction(Reduction):
 	def reduce(self, tokens, skip=0):
 		result = next(super().reduce(tokens, skip), None)
 		if result is None:
-			yield [Terminal(None), 0]
+			yield [Terminal(None)], 0
 		else:
 			yield result
 
@@ -209,19 +172,17 @@ class Rule:
 	#If the leftmost tokens match any of the Reductions,
 	#returns a Nonterminal node with corresponding to this Rule
 	#and with children corresponding to the matching Reduction.
-	def descend(self, tokens, choice):
-		if self.name == 'assignment_expression':
+	def descend(self, tokens):
+		if self.name == 'type_specifier':
 			pass
-		assert(choice < len(self.reductions))
-		
-		for reduction, length in self.reductions[choice].reduce(tokens):
-			yield [Nonterminal(self.name, reduction)], length
+		for r in self.reductions:
+			for subtree, length in r.reduce(tokens):
+				yield [Nonterminal(self.name, subtree)], length
 
 
 def parse(top_rule, tokens, **kwargs):
-	# note: top rule must have a single reduction
-	assert(len(top_rule.reductions) == 1)
-	result = next(top_rule.descend(tokens, 0), None)
+
+	result = next(top_rule.descend(tokens), None)
 
 	# TODO: add a max_tokens_consumed-type counter to the new logic
 	# TODO: the top_rule matches the empty program if something internal
